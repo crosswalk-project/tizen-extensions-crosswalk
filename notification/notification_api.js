4a17488d6142c1e4a70e8d331d5394564e724ef2
Caio Marcelo de Oliveira Filho <caio.de.oliveira.filho@intel.com>
2013-07-22 18:42:34 -0300

    Update Notification API to new JS API for Crosswalk
    The name of extension now impacts where their exposed
    functions/properties will show up, so renamed the extension to
    tizen.notification.

--- notification/notification_api.js
+++ notification/notification_api.js
@@ -2,116 +2,111 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var tizen = tizen || {};
+// FIXME(cmarcelo): Needed?
+var postedNotifications = [];
+var statusNotificationNextId = 0;
 
-(function() {
-  // FIXME(cmarcelo): Needed?
-  var postedNotifications = [];
-  var statusNotificationNextId = 0;
+var postMessage = function(msg) {
+  extension.postMessage(JSON.stringify(msg));
+};
 
-  var postMessage = function(msg) {
-    xwalk.postMessage("tizen.notifications", JSON.stringify(msg));
-  };
-
-  xwalk.setMessageListener('tizen.notifications', function(msg) {
-    var m = JSON.parse(msg);
-    if (m.cmd == "NotificationRemoved") {
-      for (var i = 0; i < postedNotifications.length; i++) {
-        if (postedNotifications[i].id === m.id) {
-          postedNotifications.splice(i, 1);
-          break;
-        }
-      }
-    }
-  });
-
-  tizen.StatusNotification = function(statusType, title, dict) {
-    this.title = title;
-    this.id = (statusNotificationNextId++).toString();
-    this.type = "STATUS";
-    if (dict) {
-      this.content = dict.content;
-    }
-  }
-
-  var copyStatusNotification = function(notification) {
-    var copy = new tizen.StatusNotification(notification.type, notification.title, { content: notification.content });
-    statusNotificationNextId--;  // Roll next id back since we are copying.
-    copy.id = notification.id;
-    return copy;
-  }
-
-  tizen.notification = {};
-  tizen.notification.post = function(notification) {
-    if (!(notification instanceof tizen.StatusNotification)) {
-      console.log("tizen.notification.post(): argument of invalid type " + typeof(notification));
-      return;
-    } else if (postedNotifications.indexOf(notification) != -1) {
-      console.log("tizen.notification.post(): notification " + notification.id + " already posted.");
-      return;
-    }
-    postMessage({
-      "cmd": "NotificationPost",
-      "id": notification.id,
-      "title": notification.title,
-      "content": notification.content,
-    });
-    postedNotifications.push(notification);
-  }
-
-  tizen.notification.remove = function(notificationId) {
-    postMessage({
-      "cmd": "NotificationRemove",
-      "id": notificationId,
-    });
-  }
-
-  tizen.notification.getAll = function() {
-    var result = [];
-    var i;
-    for (i = 0; i < postedNotifications.length; i++)
-      result[i] = copyStatusNotification(postedNotifications[i]);
-    return result;
-  }
-
-  tizen.notification.get = function(notificationId) {
-    var result;
-    var i;
-    for (i = 0; i < postedNotifications.length; i++) {
-      if (postedNotifications[i].id == notificationId) {
-        result = copyStatusNotification(postedNotifications[i]);
+extension.setMessageListener(function(msg) {
+  var m = JSON.parse(msg);
+  if (m.cmd == "NotificationRemoved") {
+    for (var i = 0; i < postedNotifications.length; i++) {
+      if (postedNotifications[i].id === m.id) {
+        postedNotifications.splice(i, 1);
         break;
       }
     }
-    return result;
   }
+});
 
-  tizen.notification.removeAll = function() {
-    var i;
-    for (i = 0; i < postedNotifications.length; i++)
-      tizen.notification.remove(postedNotifications[i].id);
+tizen.StatusNotification = function(statusType, title, dict) {
+  this.title = title;
+  this.id = (statusNotificationNextId++).toString();
+  this.type = "STATUS";
+  if (dict) {
+    this.content = dict.content;
   }
+}
 
-  tizen.notification.update = function(notification) {
-    if (!(notification instanceof tizen.StatusNotification)) {
-      console.log("tizen.notification.update(): argument of invalid type " + typeof(notification));
-      return;
+var copyStatusNotification = function(notification) {
+  var copy = new tizen.StatusNotification(notification.type, notification.title, { content: notification.content });
+  statusNotificationNextId--;  // Roll next id back since we are copying.
+  copy.id = notification.id;
+  return copy;
+}
+
+exports.post = function(notification) {
+  if (!(notification instanceof tizen.StatusNotification)) {
+    console.log("tizen.notification.post(): argument of invalid type " + typeof(notification));
+    return;
+  } else if (postedNotifications.indexOf(notification) != -1) {
+    console.log("tizen.notification.post(): notification " + notification.id + " already posted.");
+    return;
+  }
+  postMessage({
+    "cmd": "NotificationPost",
+    "id": notification.id,
+    "title": notification.title,
+    "content": notification.content,
+  });
+  postedNotifications.push(notification);
+}
+
+exports.remove = function(notificationId) {
+  postMessage({
+    "cmd": "NotificationRemove",
+    "id": notificationId,
+  });
+}
+
+exports.getAll = function() {
+  var result = [];
+  var i;
+  for (i = 0; i < postedNotifications.length; i++)
+    result[i] = copyStatusNotification(postedNotifications[i]);
+  return result;
+}
+
+exports.get = function(notificationId) {
+  var result;
+  var i;
+  for (i = 0; i < postedNotifications.length; i++) {
+    if (postedNotifications[i].id == notificationId) {
+      result = copyStatusNotification(postedNotifications[i]);
+      break;
     }
-
-    var notificationIndex = postedNotifications.indexOf(notification);
-    if (notificationIndex == -1) {
-      console.log("tizen.notification.update(): notification " + notification.id + " not yet posted.");
-      // FIXME(jeez): needed or should we post it then?
-      return;
-    }
-
-    postedNotifications[notificationIndex] = notification;
-
-    postMessage({
-      "cmd": "NotificationPost",
-      "id": notification.id,
-      "title": notification.title,
-      "content": notification.content,
-    });
   }
-})();
+  return result;
+}
+
+exports.removeAll = function() {
+  var i;
+  for (i = 0; i < postedNotifications.length; i++)
+    exports.remove(postedNotifications[i].id);
+}
+
+exports.update = function(notification) {
+  if (!(notification instanceof tizen.StatusNotification)) {
+    console.log("tizen.notification.update(): argument of invalid type " + typeof(notification));
+    return;
+  }
+
+  var notificationIndex = postedNotifications.indexOf(notification);
+  if (notificationIndex == -1) {
+    console.log("tizen.notification.update(): notification " + notification.id + " not yet posted.");
+    // FIXME(jeez): needed or should we post it then?
+    return;
+  }
+
+  postedNotifications[notificationIndex] = notification;
+
+  postMessage({
+    "cmd": "NotificationPost",
+    "id": notification.id,
+    "title": notification.title,
+    "content": notification.content,
+  });
+}
