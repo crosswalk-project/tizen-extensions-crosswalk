514f7020dab871cda79a4d9f8248b6e9faf3651d
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2013-07-29 17:21:51 +0300

    Move the check for xwalk from configure to run.sh.
    Crosswalk is a run-time dependency for tizen-extensions-crosswalk, so it is
    fine if the xwalk binary is not found at build-time.
    
    It is a real problem if it cannot be found when we need to run it, though.

--- run.sh
+++ run.sh
@@ -3,4 +3,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+if [ ! `which xwalk` ]; then
+   echo -e "\nPlease make sure xwalk is in your PATH. It is usually found at XWALK_SOURCE_DIR/../out/Release/"
+   exit 1
+fi
+
 exec xwalk "$@" --external-extensions-path=$PWD/out/Default $PWD/examples/index.html
