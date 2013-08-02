366d30ef91193d6dab5f0bf135be718631b8d986
legendlee1314 <legendlee1314@gmail.com>
2013-08-02 15:16:13 +0800

    Implement STORAGE listener

--- system_info/system_info_storage.h
+++ system_info/system_info_storage.h
@@ -5,6 +5,7 @@
 #ifndef SYSTEM_INFO_SYSTEM_INFO_STORAGE_H_
 #define SYSTEM_INFO_SYSTEM_INFO_STORAGE_H_
 
+#include <glib.h>
 #include <libudev.h>
 #include <string>
 
@@ -15,24 +16,37 @@
 class SysInfoStorage {
  public:
   static SysInfoStorage& GetSysInfoStorage(ContextAPI* api) {
-    static SysInfoStorage d(api);
-    return d;
+    static SysInfoStorage instance(api);
+    return instance;
   }
   ~SysInfoStorage();
+  // Get support
   void Get(picojson::value& error, picojson::value& data);
-  void StartListen();
-  void StopListen();
+
+  // Listerner support
+  inline void StartListen() {
+    stopping_ = false;
+    g_timeout_add_seconds(3,
+                          SysInfoStorage::TimedOutUpdate,
+                          static_cast<gpointer>(this));
+  }
+  inline void StopListen() { stopping_ = true; }
 
  private:
   explicit SysInfoStorage(ContextAPI* api);
+  bool Update(picojson::value& error);
   void GetDetails(const std::string& mnt_fsname,
                   const std::string& mnt_dir,
                   picojson::value& error,
                   picojson::value& unit);
+  static gboolean TimedOutUpdate(gpointer user_data);
   std::string GetDevPathFromMountPath(const std::string& mnt_path);
 
   ContextAPI* api_;
   struct udev* udev_;
+
+  bool stopping_;
+  picojson::value units_;
 
   DISALLOW_COPY_AND_ASSIGN(SysInfoStorage);
 };
