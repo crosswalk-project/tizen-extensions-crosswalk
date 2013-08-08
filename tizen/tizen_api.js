38b4b5035784d9e5e7883edb7220dd4e815bf7c8
Thiago Marcos P. Santos <thiago.santos@intel.com>
2013-08-08 17:18:21 +0300

    Make a enum of the possible error codes
    This is the same list as we have in JS, and they must be always in
    sync. It will be used to handle error codes in the native side.

--- tizen/tizen_api.js
+++ tizen/tizen_api.js
@@ -6,45 +6,51 @@ var postMessage = function (msg) {
     extension.postMessage(JSON.stringify(msg));
 };
 
-var errors = [
-  { type: "UNKNOWN_ERR", value: 0, name: "UnknownError", message: "" },
-  { type: "INDEX_SIZE_ERR", value: 1, name: "IndexSizeError", message: "" },
-  { type: "DOMSTRING_SIZE_ERR", value: 2, name: "DOMStringSizeError", message: "" },
-  { type: "HIERARCHY_REQUEST_ERR", value: 3, name: "HierarchyRequestError", message: "" },
-  { type: "WRONG_DOCUMENT_ERR", value: 4, name: "WrongDocumentError", message: "" },
-  { type: "INVALID_CHARACTER_ERR", value: 5, name: "IndexSizeError", message: "" },
-  { type: "NO_DATA_ALLOWED_ERR", value: 6, name: "IndexSizeError", message: "" },
-  { type: "NO_MODIFICATION_ALLOWED_ERR", value: 7, name: "IndexSizeError", message: "" },
-  { type: "NOT_FOUND_ERR", value: 8, name: "IndexSizeError", message: "" },
-  { type: "NOT_SUPPORTED_ERR", value: 9, name: "NotSupportedError", message: "" },
-  { type: "INUSE_ATTRIBUTE_ERR", value: 10, name: "IndexSizeError", message: "" },
-  { type: "INVALID_STATE_ERR", value: 11, name: "IndexSizeError", message: "" },
-  { type: "SYNTAX_ERR", value: 12, name: "IndexSizeError", message: "" },
-  { type: "INVALID_MODIFICATION_ERR", value: 13, name: "IndexSizeError", message: "" },
-  { type: "NAMESPACE_ERR", value: 14, name: "IndexSizeError", message: "" },
-  { type: "INVALID_ACCESS_ERR", value: 15, name: "IndexSizeError", message: "" },
-  { type: "VALIDATION_ERR", value: 16, name: "IndexSizeError", message: "" },
-  { type: "TYPE_MISMATCH_ERR", value: 17, name: "TypeMismatchError", message: "" },
-  { type: "SECURITY_ERR", value: 18, name: "IndexSizeError", message: "" },
-  { type: "NETWORK_ERR", value: 19, name: "IndexSizeError", message: "" },
-  { type: "ABORT_ERR", value: 20, name: "IndexSizeError", message: "" },
-  { type: "URL_MISMATCH_ERR", value: 21, name: "IndexSizeError", message: "" },
-  { type: "QUOTA_EXCEEDED_ERR", value: 22, name: "IndexSizeError", message: "" },
-  { type: "TIMEOUT_ERR", value: 23, name: "IndexSizeError", message: "" },
-  { type: "INVALID_NODE_TYPE_ERR", value: 24, name: "IndexSizeError", message: "" },
-  { type: "DATA_CLONE_ERR", value: 25, name: "IndexSizeError", message: "" },
+// WARNING! This list should be in sync with the equivalent enum
+// located at tizen.h. Remember to update tizen.h if you change
+// something here.
+var errors = {
+  "-1": { type: "NO_ERROR", name: "NoError", message: "" },
 
-  { type: "INVALID_VALUES_ERR", value: 0, name: "IndexSizeError", message: "" },
-  { type: "IO_ERR", value: 0, name: "IndexSizeError", message: "" },
-  { type: "PERMISSION_DENIED_ERR", value: 0, name: "IndexSizeError", message: "" },
-  { type: "SERVICE_NOT_AVAILABLE_ERR", value: 0, name: "IndexSizeError", message: "" },
-]
+  "0": { type: "UNKNOWN_ERR", name: "UnknownError", message: "" },
+  "1": { type: "INDEX_SIZE_ERR", name: "IndexSizeError", message: "" },
+  "2": { type: "DOMSTRING_SIZE_ERR", name: "DOMStringSizeError", message: "" },
+  "3": { type: "HIERARCHY_REQUEST_ERR", name: "HierarchyRequestError", message: "" },
+  "4": { type: "WRONG_DOCUMENT_ERR", name: "WrongDocumentError", message: "" },
+  "5": { type: "INVALID_CHARACTER_ERR", name: "InvalidCharacterError", message: "" },
+  "6": { type: "NO_DATA_ALLOWED_ERR", name: "NoDataAllowedError", message: "" },
+  "7": { type: "NO_MODIFICATION_ALLOWED_ERR", name: "NoModificationAllowedError", message: "" },
+  "8": { type: "NOT_FOUND_ERR", name: "NotFoundError", message: "" },
+  "9": { type: "NOT_SUPPORTED_ERR", name: "Not_supportedError", message: "" },
+  "10": { type: "INUSE_ATTRIBUTE_ERR", name: "InuseAttributeError", message: "" },
+  "11": { type: "INVALID_STATE_ERR", name: "InvalidStateError", message: "" },
+  "12": { type: "SYNTAX_ERR", name: "SyntaxError", message: "" },
+  "13": { type: "INVALID_MODIFICATION_ERR", name: "InvalidModificationError", message: "" },
+  "14": { type: "NAMESPACE_ERR", name: "NamespaceError", message: "" },
+  "15": { type: "INVALID_ACCESS_ERR", name: "InvalidAccessError", message: "" },
+  "16": { type: "VALIDATION_ERR", name: "ValidationError", message: "" },
+  "17": { type: "TYPE_MISMATCH_ERR", name: "TypeMismatchError", message: "" },
+  "18": { type: "SECURITY_ERR", name: "SecurityError", message: "" },
+  "19": { type: "NETWORK_ERR", name: "NetworkError", message: "" },
+  "20": { type: "ABORT_ERR", name: "AbortError", message: "" },
+  "21": { type: "URL_MISMATCH_ERR", name: "UrlMismatchError", message: "" },
+  "22": { type: "QUOTA_EXCEEDED_ERR", name: "QuotaExceededError", message: "" },
+  "23": { type: "TIMEOUT_ERR", name: "TimeoutError", message: "" },
+  "24": { type: "INVALID_NODE_TYPE_ERR", name: "InvalidNodeTypeError", message: "" },
+  "25": { type: "DATA_CLONE_ERR", name: "DataCloneError", message: "" },
+
+  // Error codes for these errors are not really defined anywhere.
+  "100": { type: "INVALID_VALUES_ERR", name: "InvalidValuesError", message: "" },
+  "101": { type: "IO_ERR", name: "IoError", message: "" },
+  "102": { type: "PERMISSION_DENIED_ERR", name: "Permission_deniedError", message: "" },
+  "103": { type: "SERVICE_NOT_AVAILABLE_ERR", name: "ServiceNotAvailableError", message: "" },
+}
 
 exports.WebAPIException = function (code, message, name) {
   var _code, _message, _name;
 
   if (typeof code !== 'number') {
-    _code = errors[0].value;
+    _code = 0;
     _message = errors[0].message;
     _name = errors[0].name;
   } else {
@@ -65,8 +71,8 @@ exports.WebAPIException = function (code, message, name) {
   this.__defineGetter__("name", function () { return _name; });
 }
 
-for (var i = 0; i < errors.length; i++)
-  Object.defineProperty(tizen.WebAPIException, errors[i].type, { value: errors[i].value });
+for (var value in errors)
+  Object.defineProperty(tizen.WebAPIException, errors[value].type, { value: value });
 
 exports.WebAPIError = function (code, message, name) {
   var _code, _message, _name;
@@ -92,6 +98,9 @@ exports.WebAPIError = function (code, message, name) {
   this.__defineGetter__("message", function () { return _message; });
   this.__defineGetter__("name", function () { return _name; });
 }
+
+for (var value in errors)
+  Object.defineProperty(tizen.WebAPIError, errors[value].type, { value: value });
 
 // NOTE: Stubs for Application. These are needed for running TCT until
 // we have a proper Application API implementation.
