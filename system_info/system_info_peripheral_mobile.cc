048f455bff724ca71ec976a4cd252ac3a8485a80
yangruiintel <rui.a.yang@intel.com>
2013-08-26 17:24:07 +0800

    [SystemInfo] implement Peripheral property

--- system_info/system_info_peripheral_mobile.cc
+++ system_info/system_info_peripheral_mobile.cc
@@ -3,12 +3,23 @@
 // found in the LICENSE file.
 
 #include "system_info/system_info_peripheral.h"
-
 #include "system_info/system_info_utils.h"
 
 void SysInfoPeripheral::Get(picojson::value& error,
                             picojson::value& data) {
-  // FIXME(halton): Add actual implementation
-  system_info::SetPicoJsonObjectValue(error, "message",
-      picojson::value("NOT IMPLEMENTED."));
+  
+ int hdmiStatus = 0, wirelessDisplayStatus = 0 ;
+ bool isVideoOutputOn=false;
+    if (vconf_get_int(VCONFKEY_MIRACAST_WFD_SOURCE_STATUS, &wirelessDisplayStatus) == 0) {
+        if(wirelessDisplayStatus==VCONFKEY_MIRACAST_WFD_SOURCE_ON) {
+                isVideoOutputOn = true;
+        }
+    }
+   if (vconf_get_int(VCONFKEY_SYSMAN_HDMI, &hdmiStatus) == 0) {
+        if(hdmiStatus==VCONFKEY_SYSMAN_HDMI_CONNECTED) {
+                isVideoOutputOn = true;
+        }
+    }
+  system_info::SetPicoJsonObjectValue(data, "isVideoOutputOn",
+     picojson::value(isVideoOutputOn));
 }
