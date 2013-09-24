bdde16b403714bb6ae65be60ae64623f1a931012
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2013-09-24 15:01:48 -0300

    [tizen] Remove not used function

--- tizen/tizen_api.js
+++ tizen/tizen_api.js
@@ -1,10 +1,6 @@
 // Copyright (c) 2013 Intel Corporation. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-var postMessage = function (msg) {
-    extension.postMessage(JSON.stringify(msg));
-};
 
 // WARNING! This list should be in sync with the equivalent enum
 // located at tizen.h. Remember to update tizen.h if you change
