632fc3cbc1a490111bc3be8787e035bc2b6b2d70
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2013-09-24 14:57:08 -0300

    [tizen] Use new C++ Wrapper instead of old ExtensionAdapter

---
+++ tizen/tizen_extension.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TIZEN_TIZEN_EXTENSION_H_
+#define TIZEN_TIZEN_EXTENSION_H_
+
+#include "common/extension.h"
+
+class TizenExtension : public common::Extension {
+ public:
+  TizenExtension();
+  virtual ~TizenExtension();
+};
+
+#endif  // TIZEN_TIZEN_EXTENSION_H_
