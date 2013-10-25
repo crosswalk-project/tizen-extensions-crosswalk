050cc36136c90b2b0fef351c188536a7ba9a4b70
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2013-10-25 19:15:40 -0200

    [Bluetooth] Add a way to validate more parameters
    In many cases, an parameter with type 'object' is passed, and this
    object is expected to have some parameters. We should be able to
    verify that this object has the correct type of attributes.
    
    Also add a simple Bluetooth address validator, using a simple regular
    expression.

--- bluetooth/bluetooth_api.js
+++ bluetooth/bluetooth_api.js
@@ -74,7 +74,7 @@ function validateArguments(signature, args) {
 
   // Mandatory arguments.
   for (var i = 0; i < mandatory_len; i++) {
-    if (typeof full_args[i] !== signature_to_type[signature[i]])
+    if (typeof full_args[i] !== signature_to_type[signature[i]] || full_args[i] === null)
       return false;
   }
 
@@ -83,6 +83,29 @@ function validateArguments(signature, args) {
     if (full_args[i] !== null && typeof full_args[i] !== signature_to_type[signature[i + 1]])
       return false;
   }
+
+  return true;
+}
+
+function validateObject(object, signature, attributes) {
+  for (var i = 0; i < signature.length; i++) {
+    if (object.hasOwnProperty(attributes[i]) &&
+        typeof object[attributes[i]] !== signature_to_type[signature[i]]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function validateAddress(address) {
+  if (typeof address !== 'string')
+    return false;
+
+  var regExp = /([\dA-F][\dA-F]:){5}[\dA-F][\dA-F]/i;
+
+  if (!address.match(regExp))
+    return false;
 
   return true;
 }
@@ -514,6 +537,11 @@ BluetoothAdapter.prototype.discoverDevices = function(discoverySuccessCallback, 
     throw new tizen.WebAPIException(tizen.WebAPIException.TYPE_MISMATCH_ERR);
   }
 
+  if (!validateObject(discoverySuccessCallback, 'ffff',
+                      ['onstarted', 'ondevicefound', 'ondevicedisappeared', 'onfinished'])) {
+    throw new tizen.WebAPIException(tizen.WebAPIException.TYPE_MISMATCH_ERR);
+  }
+
   if (adapter.checkServiceAvailability(errorCallback))
     return;
 
@@ -531,7 +559,7 @@ BluetoothAdapter.prototype.discoverDevices = function(discoverySuccessCallback, 
 
     adapter.discovery_callbacks = discoverySuccessCallback;
 
-    if (discoverySuccessCallback && typeof discoverySuccessCallback.onstarted === 'function')
+    if (discoverySuccessCallback && discoverySuccessCallback.onstarted)
       discoverySuccessCallback.onstarted();
   });
 };
@@ -579,6 +607,10 @@ BluetoothAdapter.prototype.getDevice = function(address, deviceSuccessCallback, 
     throw new tizen.WebAPIException(tizen.WebAPIException.TYPE_MISMATCH_ERR);
   }
 
+  if (!validateAddress(address)) {
+    throw new tizen.WebAPIException(tizen.WebAPIException.TYPE_MISMATCH_ERR);
+  }
+
   if (adapter.checkServiceAvailability(errorCallback))
     return;
 
@@ -595,6 +627,10 @@ BluetoothAdapter.prototype.getDevice = function(address, deviceSuccessCallback, 
 BluetoothAdapter.prototype.createBonding = function(address, successCallback, errorCallback) {
   if (!validateArguments('sf?f', arguments)) {
     throw new tizen.WebAPIError(tizen.WebAPIException.TYPE_MISMATCH_ERR);
+  }
+
+  if (!validateAddress(address)) {
+    throw new tizen.WebAPIException(tizen.WebAPIException.TYPE_MISMATCH_ERR);
   }
 
   if (adapter.checkServiceAvailability(errorCallback))
@@ -635,6 +671,10 @@ BluetoothAdapter.prototype.createBonding = function(address, successCallback, er
 BluetoothAdapter.prototype.destroyBonding = function(address, successCallback, errorCallback) {
   if (!validateArguments('s?ff', arguments)) {
     throw new tizen.WebAPIError(tizen.WebAPIException.TYPE_MISMATCH_ERR);
+  }
+
+  if (!validateAddress(address)) {
+    throw new tizen.WebAPIException(tizen.WebAPIException.TYPE_MISMATCH_ERR);
   }
 
   if (adapter.checkServiceAvailability(errorCallback))
