e254d5ac0a14e90ff207b616f9a6729871801018
Amarnath Valluri <amarnath.valluri@linux.intel.com>
2014-05-20 15:38:40 +0300

    [Speech][IVI] : Implementation of Tizen IVI Speech API.
    Following are the fixes for Tizen speech web api:
    i) Run threaded glib mainloop for Tizen.
    ii) Implements the missing speech to text api(setCBListener).
    
    BUG=XWALK-1745

--- speech/speech_instance.h
+++ speech/speech_instance.h
@@ -5,8 +5,12 @@
 #ifndef SPEECH_SPEECH_INSTANCE_H_
 #define SPEECH_SPEECH_INSTANCE_H_
 
+#include <glib.h>
+#include <thread>
+
 #include "common/extension.h"
 #include "common/picojson.h"
+#include "speech/tizen_srs_gen.h"
 
 class SpeechInstance : public common::Instance {
  public:
@@ -19,7 +23,19 @@ class SpeechInstance : public common::Instance {
   virtual void HandleSyncMessage(const char* message);
 
   const picojson::value::object
-  HandleVocalizeString(const picojson::value& msg);
+      HandleVocalizeString(const picojson::value& msg);
+  const picojson::value::object
+      HandleListenVoice(const picojson::value& msg);
+
+  static void ProxyResultCb(TizenSrs *, const gchar *const *, void*);
+
+  TizenSrs*    tts_proxy_;  // text to speech proxy
+  TizenSrs*    stt_proxy_;  // speech to text proxy
+#ifdef TIZEN
+  static void SetupMainloop(void *data);
+  GMainLoop*   main_loop_;
+  std::thread  thread_;
+#endif  // TIZEN
 };
 
 #endif  // SPEECH_SPEECH_INSTANCE_H_
